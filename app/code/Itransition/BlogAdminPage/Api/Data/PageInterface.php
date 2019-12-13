9e372392ad235c065d5cd78f89e6b41f5a42e397
a.gruzinsky <a.gruzinsky@itransition.com>
2019-12-13 17:49:09 +0300

    Training module added.

---
+++ app/code/Itransition/BlogAdminPage/Api/Data/PageInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Itransition\BlogAdminPage\Api\Data;
+
+/**
+ * Blog page interface
+ */
+interface PageInterface
+{
+    const PAGE_ID       = 'page_id';
+    const TITLE         = 'title';
+    const CONTENT       = 'content';
+    const ENABLED       = 'enabled';
+    const IDENTIFIER    = 'identifier';
+    const CREATION_TIME = 'creation_time';
+    const UPDATE_TIME   = 'update_time';
+
+}
